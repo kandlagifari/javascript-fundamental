2d94d4f820e9b8bce0df5e5066d7f5c1b139731f
Kandla Gifari Akbar <kandlagifari@gmail.com>
2023-11-01 10:13:51 +0700

    c8-completed

---
+++ 08_error-handling/02_throwing-errors.js
@@ -0,0 +1,27 @@
+const json = '{ "name": "Yoda", "age": 20 }';
+ 
+try {
+  const user = JSON.parse(json);
+ 
+  if (!user.name) {
+    throw new SyntaxError("'name' is required.");
+  }
+ 
+  errorCode;
+ 
+  console.log(user.name); // Yoda
+  console.log(user.age);  // 20
+
+// } catch (error) {
+//     console.log(`JSON Error: ${error.message}`);
+// };
+
+} catch (error) {
+    if (error instanceof SyntaxError) {
+        console.log(`JSON Error: ${error.message}`);
+    } else if (error instanceof ReferenceError) {
+        console.log(error.message);
+    } else {
+        console.log(error.stack);
+    }
+};
